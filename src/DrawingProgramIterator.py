2baa90a6e20b9c9093e2888859495012eee05e8c
Ji Lu <jilu0428@uw.edu>
2022-11-26 21:08:28 -0800

    switch name list to list_of_shapes

--- src/DrawingProgramIterator.py
+++ src/DrawingProgramIterator.py
@@ -3,10 +3,10 @@ class DrawingProgramIterator:
     """
     This class represents DrawingProgram Iterator
     """
-    def __init__(self, list):
+    def __init__(self, list_of_shapes):
         """Constructor"""
         self._index = 0
-        self.list = list
+        self.list_of_shapes = list_of_shapes
 
     def __iter__(self):
         """Creates iterator object, called when iteration is initialized"""
@@ -15,7 +15,7 @@ class DrawingProgramIterator:
     def __next__(self):
         """Method to move to next element """
         if self._index < len(self.list):
-            ret = self.list[self._index]
+            ret = self.list_of_shapes[self._index]
             self._index += 1
             return ret
         else:
